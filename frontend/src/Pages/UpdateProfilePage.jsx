2015f247feb5e5053939459a7072448402efdaa5
Tanmay Tiwari <tanmaytiwari200905@gmail.com>
2024-10-10 16:25:41 +0530

    Update issue

--- frontend/src/Pages/UpdateProfilePage.jsx
+++ frontend/src/Pages/UpdateProfilePage.jsx
@@ -67,22 +67,18 @@ const UpdateProfilePage = () => {
     try {
       // Prepare form data for multipart/form-data request
       const formData = new FormData();
-      formData.append("name", inputs.name);
-      formData.append("username", inputs.username);
-      formData.append("email", inputs.email);
-      formData.append("bio", inputs.bio);
-      if (inputs.password) formData.append("password", inputs.password);
+      Object.entries(inputs).forEach(([key, value]) => {
+        if (value) formData.append(key, value);
+      });
       if (selectedFile) formData.append("profilePic", selectedFile);
 
       const res = await fetch(`/api/users/update/${user._id}`, {
         method: "PUT",
-        body: formData, // Use FormData for file + input fields
+        body: formData,
       });
 
       const data = await res.json();
-
       if (!res.ok) {
-        // Check if the response status is not OK
         throw new Error(data.error || "Something went wrong");
       }
 
@@ -90,7 +86,7 @@ const UpdateProfilePage = () => {
       setUser(data);
       localStorage.setItem("user-spools", JSON.stringify(data));
 
-      resetForm(); // Reset form after successful update
+      resetForm();
     } catch (error) {
       console.error("Update Profile Error:", error);
       showToast(
@@ -116,13 +112,11 @@ const UpdateProfilePage = () => {
 
               {/* User Avatar */}
               <div className="flex flex-col items-center gap-4 sm:flex-row sm:justify-between">
-                <div className="flex justify-center">
-                  <img
-                    className="rounded-full object-cover w-28 h-28 sm:w-32 sm:h-32 shadow-md"
-                    src={imgUrl || user.profilePic || "defaultdp.png"}
-                    alt="User Avatar"
-                  />
-                </div>
+                <img
+                  className="rounded-full object-cover w-28 h-28 sm:w-32 sm:h-32 shadow-md"
+                  src={imgUrl || user.profilePic || "defaultdp.png"}
+                  alt="User Avatar"
+                />
                 <button
                   className="w-full md:text-base text-sm font-semibold sm:w-auto bg-gradient-to-r from-electricBlue to-softPurple text-white px-6 py-3 rounded-full hover:scale-105 shadow-xl transition-all duration-300 hover:shadow-electricBlue dark:hover:shadow-softPurple"
                   onClick={() => fileRef.current.click()}
@@ -138,129 +132,75 @@ const UpdateProfilePage = () => {
                 />
               </div>
 
-              {/* Full Name */}
-              <div className="mt-4">
-                <label
-                  htmlFor="fullName"
-                  className="block mb-2 md:text-base text-sm dark:text-white"
-                >
-                  Full Name
-                </label>
-                <input
-                  id="fullName"
-                  type="text"
-                  placeholder="John Doe"
-                  value={inputs.name}
-                  onChange={(e) =>
-                    setInputs({ ...inputs, name: e.target.value })
-                  }
-                  className="w-full p-3 md:text-base text-sm border rounded-full shadow-md dark:bg-gray-700 dark:text-white dark:border-gray-700 border-white focus:ring-2 focus:ring-gray-500 transition-transform duration-300 hover:scale-105"
-                />
-              </div>
-
-              {/* Username */}
-              <div className="mt-4">
-                <label
-                  htmlFor="username"
-                  className="block mb-2 md:text-base text-sm dark:text-white"
-                >
-                  Username
-                </label>
-                <input
-                  id="username"
-                  type="text"
-                  placeholder="johndoe"
-                  value={inputs.username}
-                  onChange={(e) =>
-                    setInputs({ ...inputs, username: e.target.value })
-                  }
-                  className="w-full p-3 md:text-base text-sm border rounded-full shadow-md dark:bg-gray-700 dark:text-white dark:border-gray-700 border-white focus:ring-2 focus:ring-gray-500 transition-transform duration-300 hover:scale-105"
-                />
-              </div>
-
-              {/* Email */}
-              <div className="mt-4">
-                <label
-                  htmlFor="email"
-                  className="block mb-2 md:text-base text-sm dark:text-white"
-                >
-                  Email Address
-                </label>
-                <input
-                  id="email"
-                  type="email"
-                  placeholder="your-email@example.com"
-                  value={inputs.email}
-                  onChange={(e) =>
-                    setInputs({ ...inputs, email: e.target.value })
-                  }
-                  className="w-full p-3 md:text-base text-sm  border rounded-full shadow-md dark:bg-gray-700 dark:text-white dark:border-gray-700 border-white focus:ring-2 focus:ring-gray-500 transition-transform duration-300 hover:scale-105"
-                />
-              </div>
-
-              {/* Bio */}
-              <div className="mt-4">
-                <label
-                  htmlFor="bio"
-                  className="block mb-2 md:text-base text-sm dark:text-white"
-                >
-                  Bio
-                </label>
-                <input
-                  id="bio"
-                  placeholder="Your bio."
-                  value={inputs.bio}
-                  onChange={(e) =>
-                    setInputs({ ...inputs, bio: e.target.value })
-                  }
-                  className="w-full p-3 md:text-base text-sm border rounded-full shadow-md dark:bg-gray-700 dark:text-white dark:border-gray-700 border-white focus:ring-2 focus:ring-gray-500 transition-transform duration-300 hover:scale-105"
-                />
-              </div>
-
-              {/* Password */}
-              <div className="mt-4">
-                <label
-                  htmlFor="password"
-                  className="block mb-2 md:text-base text-sm  dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  id="password"
-                  type="password"
-                  placeholder="Enter new password"
-                  value={inputs.password}
-                  onChange={(e) =>
-                    setInputs({ ...inputs, password: e.target.value })
-                  }
-                  className="w-full p-3 md:text-base text-sm border rounded-full shadow-md dark:bg-gray-700 dark:text-white dark:border-gray-700 border-white focus:ring-2 focus:ring-gray-500 transition-transform duration-300 hover:scale-105"
-                />
-              </div>
-
-              {/* Confirm Password */}
-              <div className="mt-4">
-                <label
-                  htmlFor="confirmPassword"
-                  className="block mb-2 md:text-base text-sm dark:text-white"
-                >
-                  Confirm Password
-                </label>
-                <input
-                  id="confirmPassword"
-                  type="password"
-                  placeholder="Confirm new password"
-                  value={inputs.confirmPassword}
-                  onChange={(e) =>
-                    setInputs({ ...inputs, confirmPassword: e.target.value })
-                  }
-                  className="w-full p-3 md:text-base text-sm border rounded-full shadow-md dark:bg-gray-700 dark:text-white dark:border-gray-700 border-white focus:ring-2 focus:ring-gray-500 transition-transform duration-300 hover:scale-105"
-                />
-              </div>
+              {/* Input fields */}
+              {[
+                {
+                  id: "fullName",
+                  label: "Full Name",
+                  value: inputs.name,
+                  placeholder: "John Doe",
+                  type: "text",
+                },
+                {
+                  id: "username",
+                  label: "Username",
+                  value: inputs.username,
+                  placeholder: "johndoe",
+                  type: "text",
+                },
+                {
+                  id: "email",
+                  label: "Email Address",
+                  value: inputs.email,
+                  placeholder: "your-email@example.com",
+                  type: "email",
+                },
+                {
+                  id: "bio",
+                  label: "Bio",
+                  value: inputs.bio,
+                  placeholder: "Your bio.",
+                  type: "text",
+                },
+                {
+                  id: "password",
+                  label: "Password",
+                  value: inputs.password,
+                  placeholder: "Enter new password",
+                  type: "password",
+                },
+                {
+                  id: "confirmPassword",
+                  label: "Confirm Password",
+                  value: inputs.confirmPassword,
+                  placeholder: "Confirm new password",
+                  type: "password",
+                },
+              ].map(({ id, label, value, placeholder, type }) => (
+                <div key={id} className="mt-4">
+                  <label
+                    htmlFor={id}
+                    className="block mb-2 md:text-base text-sm dark:text-white"
+                  >
+                    {label}
+                  </label>
+                  <input
+                    id={id}
+                    type={type}
+                    placeholder={placeholder}
+                    value={value}
+                    onChange={(e) =>
+                      setInputs({ ...inputs, [id]: e.target.value })
+                    }
+                    className="w-full p-3 md:text-base text-sm border rounded-full shadow-md dark:bg-gray-700 dark:text-white dark:border-gray-700 border-white focus:ring-2 focus:ring-gray-500 transition-transform duration-300 hover:scale-105"
+                  />
+                </div>
+              ))}
 
               {/* Action Buttons */}
               <div className="mt-8 flex justify-between">
                 <button
-                  className="bg-gradient-to-r from-red-400 to-red-500 font-semibold text-white md:text-base text-sm md:px-6 md:py-3 px-4 py-2  rounded-full hover:scale-105 shadow-xl transition-all duration-300 hover:shadow-softRed"
+                  className="bg-gradient-to-r from-red-400 to-red-500 font-semibold text-white md:text-base text-sm md:px-6 md:py-3 px-4 py-2 rounded-full hover:scale-105 shadow-xl transition-all duration-300 hover:shadow-softRed"
                   type="button"
                   onClick={resetForm}
                   disabled={updating}
